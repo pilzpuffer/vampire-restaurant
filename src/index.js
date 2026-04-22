6cc335014c340e01df970cd6554813dc402a3316
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-04-23 00:23:01 +0300

    Added basis for bat randomization for page decoration, removed unneeded bat image asset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./style.css";
+import batImage from "./assets/img/bat.svg";
+
+console.log('Hey, bloodsucker!');
+
+window.addEventListener("load", function() {
+
+    let content = document.querySelector("#content");
+    let batVariety = ["gray-filter", "rotate", "wisp", "ghost", "ghoul"];
+
+    for (let i = 0; i < 10; i++) {
+        let bat = document.createElement("img");
+        let selectVariant = batVariety[Math.floor(Math.random() * batVariety.length)];
+        console.log(selectVariant);
+        let chance = Math.random();
+        bat.src = batImage;
+        bat.classList.add("bat");
+            if ( chance > 0.5 ) {
+                bat.classList.add(`${selectVariant}`);
+            }
+
+        content.appendChild(bat);
+    } 
+})
+
