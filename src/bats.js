08d56992a87030f1217e9ba42bfbbeae6ca05d8d
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-04-25 03:16:49 +0300

    Improve bat creation function by preventing duplicate/similiar trait assignment

--- src/bats.js
+++ src/bats.js
@@ -12,26 +12,50 @@ let batCloud = function () {
 
     for (let i = 0; i < 10; i++) {
         let bat = document.createElement("img");
+        bat.src = batImage;
+        bat.classList.add("bat");
+
         let categorySelect;
+        let chance = Math.random();
         let selectVariant = function() {
             categorySelect = batVariety[Math.floor(Math.random() * batVariety.length)];
-            console.log(batVariety.indexOf(categorySelect));
             return categorySelect[Math.floor(Math.random() * categorySelect.length)];
         }
 
         let currentPick = selectVariant();
-        let chance = Math.random();
-        bat.src = batImage;
-        bat.classList.add("bat");
-            if ( chance > 0.4 ) {
-                if (bat.classList.contains(`${currentPick}`) && !bat.classList.contains(batVariety.indexOf(categorySelect))) {
-                    console.log (`current pick is ${currentPick}, this is bat number ${i}`)
+
+        let duplicatePrevent = function() {
+            let currTrait = [];
+            let newTrait = [];
+            for ( let i = 0; i < batVariety.length; i++ ) {
+                for ( let j = 0; j < batVariety[i].length; j++ ) {
+                    if ( batVariety[i][j] === bat.classList[1] ) {
+                        currTrait = [i, j];
+                    }
+
+                    if ( batVariety[i][j] === `${currentPick}` ) {
+                        newTrait = [i, j];
+                    }
                 }
-                bat.classList.contains(`${currentPick}`) ? bat.classList.add(`${selectVariant()}`) : bat.classList.add(`${currentPick}`);
-                if (chance > 0.7) {
-                    bat.classList.contains(`${currentPick}`) ? bat.classList.add(`${selectVariant()}`) : bat.classList.add(`${currentPick}`);
-                } 
+
+                if ( newTrait.length === 2 && currTrait.length === 2 ) {
+                    if ( newTrait.toString() === currTrait.toString() || newTrait[0] === currTrait[0] ) {
+                        currentPick = selectVariant();
+                        duplicatePrevent();
+                    } else {
+                        bat.classList.add(`${currentPick}`);
+                    }
+                }
             }
+        }   
+        
+        if ( chance > 0.4 ) {
+            bat.classList.add(currentPick);
+        }
+
+        if ( chance > 0.7 ) {
+            duplicatePrevent();
+        } 
 
         content.appendChild(bat);
     } 
