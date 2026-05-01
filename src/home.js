7d44692c79106eab96a5768bd12e1bd39d5623bb
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-05-01 04:14:59 +0300

    Add homepage text and a way to access menu through it

--- src/home.js
+++ src/home.js
@@ -2,9 +2,34 @@ let homeDisplay = function() {
     let content = document.querySelector("#content");
 
     let homeBlock = document.createElement("div");
-    homeBlock.textContent = 'Welcome in, our dear guest!';
     homeBlock.classList.add("home-text");
-    content.appendChild(homeBlock);
+
+    let addLine = function(text) {
+        let newLine = document.createElement("div");
+        newLine.textContent = text;
+
+        homeBlock.appendChild(newLine);
+    }
+
+    addLine("Come in, dear guest, and be welcome.");
+    addLine("We serve those who wander after dusk - and those who find themselves a little lost.");
+    addLine("At The Last Drop, we take pride in every detail, from the first sip to the final bite, best savored long after sunset.");
+    addLine("Stay as long as you like. There’s no need to rush the night.");
+    addLine("Step in, and take a bite.");
+
+    homeBlock.addEventListener("mouseover", function(event) {
+        if (event.target === homeBlock.lastChild) {
+            homeBlock.lastChild.style.fontWeight = "bold";
+        } else {
+            homeBlock.lastChild.style.fontWeight = ""
+        }
+    })
+
+    homeBlock.lastChild.addEventListener("click", function() {
+        document.getElementById('menu').click();
+    })
+
+    content.appendChild(homeBlock);  
 }
 
 export { homeDisplay };
